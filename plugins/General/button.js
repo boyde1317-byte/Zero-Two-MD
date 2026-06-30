da271786d449241ed55ec2612d05ae7e025f4b10
boyde1317-byte <boyde1317@gmail.com>
2026-06-30 18:12:40 +0000

    Update plugins/General/button.js - Zero Two MD branding

--- plugins/General/button.js
+++ plugins/General/button.js
@@ -12,7 +12,7 @@ export default {
       await client.sendMessage(m.chat, {
         text: `╭─❏ 「 Mᴇɴᴜ」
 │ Choose an option from the list:\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`,
-        footer: 'Toxic-MD Bot',
+        footer: 'Zero Two Bot',
         sections: [
           {
             title: 'General Commands',
