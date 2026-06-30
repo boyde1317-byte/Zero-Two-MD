e79a7ef0f4caca7f5040ae630f3b418d1c941b8c
boyde1317-byte <boyde1317@gmail.com>
2026-06-30 18:13:12 +0000

    Update plugins/Owner/restart.js - Zero Two MD branding

--- plugins/Owner/restart.js
+++ plugins/Owner/restart.js
@@ -7,7 +7,7 @@ export default async (context) => {
         await client.sendMessage(m.chat, { react: { text: '🔄', key: m.reactKey } });
         await client.sendMessage(m.chat, { react: { text: '⌛', key: m.reactKey } });
         await sendInteractive(client, m, `╭─❏ 「 RESTART」
-│ Restarting Toxic-MD...\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`);
+│ Restarting Zero Two...\n╰───────────────\n> ©𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧`);
         setTimeout(() => { process.exit(0); }, 3000);
     });
 };
